175843e6026092cf24a178c95d24ff43efdff5bd
lucas <dantaslucas337@gmail.com>
2022-10-31 15:53:42 -0300

    Exercicio 6

--- Exercicio5/src/Component/Product/Products.js
+++
@@ -1,8 +0,0 @@
-const products = [
-    { name: "Bolo de aniversário", icon: "🎂", price: 20.9 },
-    { name: "Balão", icon: "🎈", price: 5.6 },
-    { name: "Confete", icon: "🎉", price: 2.9 },
-    { name: "Suco de caixinha", icon: "🧃", price: 1.9 },
-    { name: "Doces diversos", icon: "🍬", price: 12.3 }
-];
-export default products
